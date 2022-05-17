11d58decbc94b9aee38b0a363622f2ec68aa8a58
amancoder <amancoder28@gmail.com>
2022-05-17 17:05:28 +0000

    Feat: Switch Based Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { $ } from "voby";
+import { $, Switch } from "voby";
 import banner from "/banner.svg";
 
 function App(): JSX.Element {
-  const count = $(0);
-  const increment = () => count((value) => value + 1);
+  const route = $("/");
 
   return (
     <main class="bg-[#fff6db] text-[#2f2f2f] w-screen h-screen flex-col flex-center">
       <img src={banner} class="h-[60vmin] pointer-events-none" alt="logo" />
       <div class="mt-4 flex-center flex-col py-4">
-        <section class="mb-4">
-          <button
-            class="py-2 px-4 rounded-lg shadow-lg text-center text-white text-lg font-normal bg-emerald-700 hover:focus:(ring-4 ring-emerald-400) transition-all"
-            style="border: none"
-            onClick={increment}
-          >
-            count is: {count}
+        <Switch when={route}>
+          <Switch.Case when={"/"}>
+            <h1 class="text-2xl">
+              Home Page
+            </h1>
+          </Switch.Case>
+          <Switch.Case when={"/playground"}>
+            <h1 class="text-2xl">
+              Playground Page
+            </h1>
+          </Switch.Case>
+        </Switch>
+        <section class="mt-8"> 
+          <button class="py-2 px-4 rounded-lg shadow-lg text-center text-white text-lg font-normal bg-emerald-700 hover:focus:(ring-4 ring-emerald-400) transition-all"
+            style="border: none" onClick={() => route() === "/" ? route("/playground") : route("/")}>
+              Route to {() => route() === "/" ? "/playground" : "/"}
           </button>
-        </section>
-        <p class="text-2xl mt-4 mx-4">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <section class="mt-4">
-          <a
-            class="text-[#2f2f2f] text-2xl font-normal underline underline-offset-2"
-            href="https://github.com/vobyjs/voby"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Voby
-          </a>
         </section>
       </div>
     </main>
