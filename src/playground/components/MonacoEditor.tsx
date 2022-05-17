484f9ce665113e232c8890b011c7be527b50353f
LennyAnders <hey@lenny.fyi>
2022-05-17 22:16:21 +0200

    make jsx with voby/oby types work in monaco

--- src/playground/components/MonacoEditor.tsx
+++ src/playground/components/MonacoEditor.tsx
@@ -1,4 +1,4 @@
-import { editor, languages } from "monaco-editor";
+import { editor, languages, Uri } from "monaco-editor";
 // @ts-ignore
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 // @ts-ignore
@@ -14,8 +14,22 @@ import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker"
     }
   },
 };
+let vobyFiles = import.meta.globEager("/node_modules/voby/dist/**/*", { as: "raw" });
+Object.assign(vobyFiles, import.meta.globEager("/node_modules/voby/package.json", { as: "raw" }));
+Object.assign(vobyFiles, import.meta.globEager("/node_modules/oby/dist/**/*", { as: "raw" }));
+Object.assign(vobyFiles, import.meta.globEager("/node_modules/oby/package.json", { as: "raw" }));
+for (const path in vobyFiles) {
+  editor.createModel(vobyFiles[path] as any as string, "typescript", Uri.parse(`file://${path}`));
+}
+languages.typescript.typescriptDefaults.setEagerModelSync(true);
 languages.typescript.typescriptDefaults.setCompilerOptions({
+  strict: true,
+  target: languages.typescript.ScriptTarget.ESNext,
+  module: languages.typescript.ModuleKind.ESNext,
+  moduleResolution: languages.typescript.ModuleResolutionKind.NodeJs,
   jsx: languages.typescript.JsxEmit.Preserve,
+  jsxImportSource: "voby",
+  allowNonTsExtensions: true,
 });
 
 import { $, useEffect, useSample } from "voby";
