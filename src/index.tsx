f14b8da5434d61fd5355f4be00e2ba26f5b8ab80
Aman_Coder <60542770+amancoder28@users.noreply.github.com>
2022-05-18 00:45:42 +0300

    Refactor Imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 /* @refresh reload */
 import { render, lazy } from "voby";
 
-import "./styles/global.css";
 import "uno.css";
 import "@unocss/reset/tailwind.css";
+import "./styles/global.css";
+
 import { default as _App } from "./App";
 import { LazyComponent } from "voby/dist/types";
 
 const Playground = lazy(() => import("./playground"));
-
 let App: JSX.Element | LazyComponent = _App;
+
 switch (location.pathname) {
   case "/playground":
     App = Playground;
