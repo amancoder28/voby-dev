b4d508a05a43d5350f70ccfbeb8a7632d77daa21
LennyAnders <hey@lenny.fyi>
2022-05-21 16:40:35 +0200

    hopefully fix spa on gh-pages 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,4 @@ import UnocssPlugin from "unocss/vite";
 export default defineConfig({
   base: "/voby-dev/",
   plugins: [voby(), UnocssPlugin({})],
-  build: {
-    rollupOptions: {
-      input: ["index.html", "404.html"],
-    },
-  },
 });
