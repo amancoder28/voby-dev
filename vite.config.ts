b475575556bef2b306b87be7ddf911b019cfce5f
LennyAnders <hey@lenny.fyi>
2022-05-21 16:22:18 +0200

    add 404.html for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import UnocssPlugin from "unocss/vite";
 export default defineConfig({
   base: "/voby-dev/",
   plugins: [voby(), UnocssPlugin({})],
+  build: {
+    rollupOptions: {
+      input: ["index.html", "404.html"],
+    },
+  },
 });
