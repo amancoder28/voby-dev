73636ebd291eba2c27b5f86b9a65d787c1e2285c
Aman_Coder <60542770+amancoder28@users.noreply.github.com>
2022-05-22 18:47:14 +0300

    Create strengths.ts

---
+++ src/components/Hero/strengths.ts
@@ -0,0 +1,26 @@
+export const strengths = [
+  {
+    icon: "https://fonts.gstatic.com/s/i/materialiconsoutlined/trending_up/v19/24px.svg",
+    label: "Performant",
+    description:
+      "Truly reactive, compiler-optimized rendering system that speedy and memory efficient.",
+  },
+  {
+    icon: "https://fonts.gstatic.com/s/i/materialiconsoutlined/star/v20/24px.svg",
+    label: "Reactive",
+    description:
+      "Composable reactive observable based primitives joined with the flexibility of JSX."
+  },
+  {
+    icon: "https://fonts.gstatic.com/s/i/materialicons/tune/v12/24px.svg",
+    label: "Productive",
+    description:
+      "Builds on top of standard Observable & JSX based intuitive API and world-class Performance."
+  },
+  {
+    icon: "https://fonts.gstatic.com/s/i/materialiconsoutlined/speed/v12/24px.svg",
+    label: "Efficient",
+    description:
+      "Extremely reactive rendering System with minimal bundle sizes & no overheads.",
+  },
+];
