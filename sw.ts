a206c2452372ec292ae948a4e3a0010dec3eae55
amancoder28 <amancoder28@gmail.com>
2022-05-23 12:36:22 -0800

    Feat: Cache Esbuild Wasm

--- sw.ts
+++ sw.ts
@@ -1,18 +1,29 @@
 import { precacheAndRoute } from "workbox-precaching";
-// import { registerRoute } from "workbox-routing";
-// import { CacheFirst } from "workbox-strategies";
+import { registerRoute } from "workbox-routing";
+import { CacheFirst } from "workbox-strategies";
 
-import { googleFontsCache, imageCache, pageCache } from "workbox-recipes";
+import { googleFontsCache, imageCache, pageCache, staticResourceCache } from "workbox-recipes";
 
 declare let self: ServiceWorkerGlobalScope;
 
 pageCache();
 googleFontsCache();
 imageCache();
+staticResourceCache();
 
-// registerRoute(
-//   new RegExp("https://my-voby-app.com/.*\\.(ttf|woff|woff2)"),
-//   new CacheFirst({ cacheName: "fonts" })
-// );
+registerRoute(
+  new RegExp("http://localhost:5000/voby-dev/.*\\.(wasm|ttf|woff|woff2)"),
+  new CacheFirst({ cacheName: "big-assets-cache" }),
+);
+
+registerRoute(
+  new RegExp("https://cdn.skypack.dev/.*\\.(js|css|html)"),
+  new CacheFirst({ cacheName: "skypack-cache" }),
+);
+
+registerRoute(
+  new RegExp("https://unpkg.com/.*\\.(js|css|html)"),
+  new CacheFirst({ cacheName: "unpkg-cache" }),
+);
 
 precacheAndRoute(self.__WB_MANIFEST);
