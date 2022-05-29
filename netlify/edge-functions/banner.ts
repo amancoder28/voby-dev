92c291ccf6c4c3b71f4415a103dcd570a2f38ba3
Aman_Coder <60542770+amancoder28@users.noreply.github.com>
2022-05-29 03:48:03 -0800

    Fix: Change to Png Image

--- netlify/edge-functions/banner.ts
+++ netlify/edge-functions/banner.ts
@@ -17,7 +17,7 @@ export default async (request: Request, { json }: Context) => {
   const bannerType = imageTypes[type] ?? "banner";
 
   const vobyImage = await fetch(
-    `https://raw.githubusercontent.com/vobyjs/voby/master/resources/banner/svg/${bannerType}.svg`,
+    `https://raw.githubusercontent.com/vobyjs/voby/master/resources/banner/png/${bannerType}.png`,
   );
 
   return cors(
@@ -25,7 +25,7 @@ export default async (request: Request, { json }: Context) => {
     new Response(vobyImage.body, {
       headers: {
         "cache-control": "public, max-age=86400, stale-while-revalidate=86400",
-        "content-type": "image/svg+xml"
+        "content-type": "image/png"
       },
     }),
   );
