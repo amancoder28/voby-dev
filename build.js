3ee641bcf0891aa02af6ad25b7eef65331ab5316
Aman_Coder <60542770+amancoder28@users.noreply.github.com>
2022-06-10 19:30:12 +0300

    Create build.js

---
+++ build.js
@@ -0,0 +1,13 @@
+import { build } from 'esbuild';
+
+(async function main() {
+  await build({
+    entryPoints: ['./sw.ts'],
+    bundle: true,
+    platform: 'browser',
+    target: ['chrome95', 'es2020'],
+    minify: true,
+    outfile: 'dist/service-worker.js',
+    treeShaking: true
+  });
+})();
